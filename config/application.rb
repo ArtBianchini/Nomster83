776e10ed8bf07a7acee6c60c5f3851189f616d91
Arthur Bianchini <ArtBianchini@gmail.com>
2019-10-08 16:48:15 +0000

    L11 done

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../'boot', __FILE__)
 
 require 'rails/all'
 
@@ -9,11 +9,12 @@ Bundler.require(*Rails.groups)
 module Nomster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.assets.paths <<"#{Rails.root}/app/assets/fonts"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+
+# Precompile additional assets
+config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+
   end
 end
