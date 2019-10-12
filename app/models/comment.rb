6b75fae01e39526f4602dccf2b96bd2f750c7936
Arthur Bianchini <ArtBianchini@gmail.com>
2019-10-12 00:38:17 +0000

    L35 done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
+
+
+	Ratings = {
+		'one star': '1_star',
+		'two stars': '2_stars',
+		"three stars": '3_stars',
+		'four stars': '4_stars',
+		'five stars': '5 stars', 
+	}
+
+
+	def humanized_ratings
+		RATINGS.invert[self.rating]
+  end
 end
